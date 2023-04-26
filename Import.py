6f00e6a477a355a1af9bb11d19d69331aedb1c97
Lukas Behammer <lukas190101@gmail.com>
2023-04-26 17:06:52 +0200

    Change Import.py

--- Import.py
+++ Import.py
@@ -3,32 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 # import glob
 
-# path = "./Data/*"
-# files = glob.glob(path)
-# file = files[0]
+
 
 
 def img_data_loader(file):
     img = nib.load(file)
     # shape = img.header.get_data_shape()
     img_data = img.get_fdata()
-    return img_data
+    return img, img_data
 
 
-def plot_in_orientation(img_data, orientation, slice):
-    img_data = img_data[:, :, :, 0]
+def plot_in_orientation(img_data, orientation, slice, cmap="gray", ax=None):
     if orientation == "coronal":
         coronal = np.transpose(img_data, [0, 2, 1])
         coronal = np.rot90(coronal, 1)
-        plt.imshow(coronal[:, :, slice], cmap="gray")
+        ax.imshow(coronal[:, :, slice], cmap=cmap) if ax else plt.imshow(coronal[:, :, slice], cmap=cmap)
     elif orientation == "sagittal":
         sagittal = np.transpose(img_data, [1, 2, 0])
         sagittal = np.rot90(sagittal, 1)
-        plt.imshow(sagittal[:, :, slice], cmap="gray")
+        ax.imshow(sagittal[:, :, slice], cmap=cmap) if ax else plt.imshow(sagittal[:, :, slice], cmap=cmap)
     elif orientation == "transversal":
         transversal = np.transpose(img_data, [0, 1, 2])
         transversal = np.rot90(transversal, 1)
-        plt.imshow(transversal[:, :, slice], cmap="gray")
+        ax.imshow(transversal[:, :, slice], cmap=cmap) if ax else plt.imshow(transversal[:, :, slice], cmap=cmap)
     else:
         print("No valid orientation found!")
-    plt.show()
+    if not ax:
+        plt.show()
