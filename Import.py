730e06e783c0dcea6390636e3b2fbbc0e8a1ad61
lukasbehammer <lukas190101@gmail.com>
2023-05-08 12:03:41 +0200

    move atlas acquisition from Main.py to Import.py

--- Import.py
+++ Import.py
@@ -1,9 +1,8 @@
 import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
-# import glob
-
-
+from nilearn import datasets
+from nilearn.regions import connected_label_regions
 
 
 def img_data_loader(file):
@@ -30,3 +29,25 @@ def plot_in_orientation(img_data, orientation, slice, cmap="gray", ax=None):
         print("No valid orientation found!")
     if not ax:
         plt.show()
+
+
+def get_parcellation_data(parcel_dir='./resources/rois/', fetched=False):
+    # fetch parcellations
+    if fetched is True:
+        region_labels = nib.load('./resources/rois/aal_SPM12/aal/ROI_MNI_V4.nii')   # load atlas if already fetched
+    else:
+        atlas_aal_SPM12 = datasets.fetch_atlas_aal(data_dir=parcel_dir)   # fetch aal atlas
+        region_labels = connected_label_regions(atlas_aal_SPM12['maps'])   # get region labels as nifti from atlas
+    region_labels_data = region_labels.get_fdata()   # get image data from region labels
+
+    # mask regions
+    masked_aal = np.ma.masked_where(region_labels_data == 0, region_labels_data)   # mask no brain regions
+
+    # make array in which each region has 0 or 1 for each voxel in 3D space
+    regions = np.unique(masked_aal)[0:-1]   # get list of all region labels
+    binary_region_list = []
+    for region in regions:
+        binary_region = masked_aal == region   # compute boolean value for each voxel per region
+        binary_region_list.append(binary_region)
+
+    return region_labels, regions, binary_region_list
--- Main.py
+++ Main.py
@@ -1,28 +1,9 @@
-from nilearn import datasets
-from nilearn.regions import connected_label_regions
 import nibabel as nib
 import numpy as np
-
-# fetch parcellations
-parcel_dir = './resources/rois/'    # set directory for atlas
-atlas_aal_SPM12 = datasets.fetch_atlas_aal(data_dir=parcel_dir) # fetch aal atlas
-region_labels = connected_label_regions(atlas_aal_SPM12['maps'])    # get region labels as nifti from atlas
-# region_labels = nib.load('./resources/rois/aal_SPM12/aal/ROI_MNI_V4.nii') # load atlas if already fetched
-region_labels_data = region_labels.get_fdata()  # get image data from region labels
-
-# mask regions
-masked_aal = np.ma.masked_where(region_labels_data == 0, region_labels_data)    # mask no brain regions
 
 # load specific fMRI Image
 path_fMRI = "./Data/rfMRI_REST1_LR_hp2000_clean.nii.gz"
 img_fMRI = nib.load(path_fMRI)
-
-# make array in which each region has 0 or 1 for each voxel in 3D space
-regions = np.unique(masked_aal)[0:-1]   # get list of all region labels
-binary_region_list = []
-for region in regions:
-    binary_region = masked_aal == region    # compute boolean value for each voxel per region
-    binary_region_list.append(binary_region)
 
 mean_intensity_per_region_array = []
 shape = img_fMRI.header.get_data_shape()    # get shape for amount of timestamps
