b2ba233d3716f85c668fc2d902978ba0ddf3702b
lukasbehammer <lukas190101@gmail.com>
2023-05-08 12:27:39 +0200

    rewrite timeseries acquisition as function

--- Main.py
+++ Main.py
@@ -1,29 +1,41 @@
+from Import import get_parcellation_data
 import nibabel as nib
 import numpy as np
+import os
 
-# load specific fMRI Image
-path_fMRI = "./Data/rfMRI_REST1_LR_hp2000_clean.nii.gz"
-img_fMRI = nib.load(path_fMRI)
 
-mean_intensity_per_region_array = []
-shape = img_fMRI.header.get_data_shape()    # get shape for amount of timestamps
+def get_timeseries(patient):
+    # load specific fMRI Image
+    path_fMRI = "./Data/rfMRI_REST1_LR_hp2000_clean.nii.gz"
+    img_fMRI = nib.load(path_fMRI)
 
-# resample fMRI image to region labels
-# resampled_img_fMRI = nimg.resample_to_img(img_fMRI, region_labels, interpolation='nearest')
-resampled_img_fMRI = img_fMRI   # HCP fMRI image has already same size as region labels
+    # load atlas data
+    region_labels, masked_aal, regions, binary_region_list = get_parcellation_data(fetched=True)
 
-# compute activity time series
-for timestamp in np.arange(shape[3]):
+    mean_intensity_per_region_array = []
+    shape = img_fMRI.header.get_data_shape()  # get shape for amount of timestamps
 
-    # resample Image
-    resampled_img_data_fMRI = resampled_img_fMRI.dataobj[:, :, :, timestamp]    # get data for timestamp
+    # resample fMRI image to region labels
+    # resampled_img_fMRI = nimg.resample_to_img(img_fMRI, region_labels, interpolation='nearest')
+    resampled_img_fMRI = img_fMRI  # HCP fMRI image has already same size as region labels
 
-    # calculate mean intensity per region and timestamp
-    mean_intensity_per_region_list = []
-    for region in regions:
-        mean_intensity_per_region_list.append(np.mean(resampled_img_data_fMRI[binary_region_list[int(region)-1]]))  #
-        # compute mean intensity per region
-    mean_intensity_per_region_array.append(mean_intensity_per_region_list)  # add mean intensity per region for this
-    # timestamp to time series
+    # compute activity time series
+    for timestamp in np.arange(shape[3]):
 
-np.save('./Data/mean_intensity_per_region.npy', mean_intensity_per_region_array)    # save time series as numpy object
+        # resample Image
+        resampled_img_data_fMRI = resampled_img_fMRI.dataobj[:, :, :, timestamp]  # get data for timestamp
+
+        # calculate mean intensity per region and timestamp
+        mean_intensity_per_region_list = []
+        for region in regions:
+            mean_intensity_per_region_list.append(np.mean(resampled_img_data_fMRI[binary_region_list[int(region)-1]]))
+            # compute mean intensity per region
+        mean_intensity_per_region_array.append(mean_intensity_per_region_list)  # add mean intensity per region for this
+        # timestamp to time series
+
+    filename = f"{patient}_timeseries.npy"
+    path = "./Data"
+    file = os.path.join(path, filename)
+    np.save(file, mean_intensity_per_region_array)  # save time series as numpy object
+
+    return mean_intensity_per_region_array
--- Visualization.py
+++ Visualization.py
@@ -1,30 +1,13 @@
 from Import import *
-from nilearn import datasets
-from nilearn.regions import connected_label_regions
 from nilearn import image as nimg
-import nibabel as nib
 import numpy as np
 
-# fetch parcellations
-parcel_dir = './resources/rois/'    # set directory for atlas
-atlas_aal_SPM12 = datasets.fetch_atlas_aal(data_dir=parcel_dir) # fetch aal atlas
-region_labels = connected_label_regions(atlas_aal_SPM12['maps'])    # get region labels as nifti from atlas
-# region_labels = nib.load('./resources/rois/aal_SPM12/aal/ROI_MNI_V4.nii') # load atlas if already fetched
-region_labels_data = region_labels.get_fdata()  # get image data from region labels
-
-# mask regions
-masked_aal = np.ma.masked_where(region_labels_data == 0, region_labels_data)    # mask no brain regions
+# load atlas data
+region_labels, masked_aal, regions, binary_region_list = get_parcellation_data(fetched=True)
 
 # load specific fMRI Image
 path_fMRI = "./Data/rfMRI_REST1_LR_hp2000_clean.nii.gz"
 img_fMRI = nib.load(path_fMRI)
-
-# make array in which each region has 0 or 1 for each voxel in 3D space
-regions = np.unique(masked_aal)[0:-1]   # get list of all region labels
-binary_region_list = []
-for region in regions:
-    binary_region = masked_aal == region    # compute boolean value for each voxel per region
-    binary_region_list.append(binary_region)
 
 # load Average T1w Image
 path_T1w = "./Data/S1200_AverageT1w_restore.nii.gz"
@@ -33,7 +16,7 @@ img_T1w, img_data_T1w = img_data_loader(path_T1w)
 # resample Images to region labels
 resampled_img_data_T1w = nimg.resample_to_img(img_T1w, region_labels, interpolation='nearest').get_fdata()
 # resampled_img_fMRI = nimg.resample_to_img(img_fMRI, region_labels, interpolation='nearest')
-resampled_img_fMRI = img_fMRI   # HCP fMRI image has already same size as region labels
+resampled_img_fMRI = img_fMRI  # HCP fMRI image has already same size as region labels
 
 # plot fMRI-image with parcellation
 fig, ax = plt.subplots()
