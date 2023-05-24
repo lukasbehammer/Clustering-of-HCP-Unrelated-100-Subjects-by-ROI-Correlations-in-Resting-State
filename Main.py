7fc13f0adfdf3d12fb7fd9dd26b648b3a59239ae
Lukas Behammer <lukas190101@gmail.com>
2023-05-24 20:30:55 +0200

    add centroid and network graph generation

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ import numpy as np
 import os
 
 
-def get_timeseries_per_patient(patient_id, scan_num, path="./Data/rfMRI_REST1_LR_hp2000_clean.nii.gz"):
+def get_timeseries_per_patient(patient_id, scan_num, path="N:/HCP/Unrelated 100/Patients"):
     # load specific fMRI Image
     scan_names = ["rfMRI_REST1_LR", "rfMRI_REST1_RL", "rfMRI_REST2_LR", "rfMRI_REST2_RL"]
     file_names = ["rfMRI_REST1_LR_hp2000_clean.nii.gz", "rfMRI_REST1_RL_hp2000_clean.nii.gz",
@@ -40,9 +40,8 @@ def get_timeseries_per_patient(patient_id, scan_num, path="./Data/rfMRI_REST1_LR
         # timestamp to time series
 
     filename = f"patient-{patient_id}_scan-{scan_num}_timeseries.npy"
-    path = "./Data/timeseries"
-    file = os.path.join(path, filename)
-    np.save(file, mean_intensity_per_region_array)  # save time series as numpy object
+    file = os.path.join(path, "timeseries", filename)
+    np.save(file=file, arr=mean_intensity_per_region_array)  # save time series as numpy object
 
     return mean_intensity_per_region_array
 
@@ -50,14 +49,41 @@ def get_timeseries_per_patient(patient_id, scan_num, path="./Data/rfMRI_REST1_LR
 def timeseries_pearson_corr(arr_1, step_width=12, overlap_percentage=0.2):
     shape = arr_1.shape
     offset = int(overlap_percentage * step_width)
-    connectomics = []
+    correlation_matrix = []
     start = 0
     while True:
         stop = start + step_width
         if stop > shape[0]:
             break
         r = np.corrcoef(arr_1[start:stop, :], rowvar=False)
-        connectomics.append(r)
+        correlation_matrix.append(r)
         start = stop - offset
 
-    return connectomics
+    return correlation_matrix
+
+
+def get_centroid(coordinate_list):
+    length = len(coordinate_list)
+    sum_x = np.sum([coordinate_list[item][0] for item in np.arange(length)])
+    sum_y = np.sum([coordinate_list[item][1] for item in np.arange(length)])
+    sum_z = np.sum([coordinate_list[item][2] for item in np.arange(length)])
+    return sum_x/length, sum_y/length, sum_z/length
+
+
+def get_centroids_per_region(region_labels_data, regions, region_from, region_to):
+    regions = regions[region_from:region_to]
+    centroids = []
+    for region in regions:
+        coordinate_list = []
+        for x in np.arange(region_labels_data.shape[0]):
+            for y in np.arange(region_labels_data.shape[1]):
+                for z in np.arange(region_labels_data.shape[2]):
+                    coordinate_list.append((x, y, z) if region_labels_data[x, y, z] == region else None)
+        while None in coordinate_list:
+            coordinate_list.remove(None)
+        centroids.append(get_centroid(coordinate_list))
+    path = "./Data"
+    filename = f"centroids-for-regions-{region_from}-{region_to}.npy"
+    file = os.path.join(path, filename)
+    np.save(file=file, arr=centroids)
+    return centroids
